b3b368f4989dd9d7e0144a3611ead5a4fc20d510
ismdeep <ismdeep@live.com>
2014-08-22 21:44:32 +0800

    update
    update

--- src/com/lemox/game2048/MainActivity.java
+++ src/com/lemox/game2048/MainActivity.java
@@ -1,7 +1,6 @@
 package com.lemox.game2048;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
